c966bfdc1c067e750d9c9d7e9bcbf4af55d347f8
Kayla Cohn <kayla.s.cohn@gmail.com>
2016-04-06 20:50:23 -0400

    add read option to open method

--- palindrome/palindrome.rb
+++ palindrome/palindrome.rb
@@ -1,4 +1,4 @@
-File.open("../Ruby/palindrome/names.txt") do |file|
+File.open("../Ruby/palindrome/names.txt", "r") do |file|
   palindrome = file.read.split("\n").select do |name|
     name == name.reverse
   end
