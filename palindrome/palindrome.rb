8856d1fef0f8f01bef107e68f30b96f94cb9925f
Kayla Cohn <kayla.s.cohn@gmail.com>
2016-04-06 20:37:44 -0400

    removed class

--- palindrome/palindrome.rb
+++ palindrome/palindrome.rb
@@ -1,13 +1,6 @@
-class Palindrome
-
-  def initialize
-    File.open("../Ruby/palindrome/names.txt") do |file|
-      palindrome = file.read.split("\n").select do |name|
-        name == name.reverse
-      end
-      puts palindrome
-    end
+File.open("../Ruby/palindrome/names.txt") do |file|
+  palindrome = file.read.split("\n").select do |name|
+    name == name.reverse
   end
+  puts palindrome
 end
-
-Palindrome.new
